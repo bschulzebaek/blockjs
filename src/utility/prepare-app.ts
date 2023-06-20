fa5f6de510fbf39bce2f4e29135d7591e4871a03
Benedikt Schulze Baek <b.schulze-baek@shopware.com>
2023-06-20 19:36:10 +0200

    Remove unused setup function

--- src/utility/prepare-app.ts
+++
@@ -1,8 +0,0 @@
-import { Application, FILLMODE_FILL_WINDOW, RESOLUTION_AUTO } from 'playcanvas';
-
-export default function prepareApp(app: Application) {
-    app.setCanvasFillMode(FILLMODE_FILL_WINDOW);
-    app.setCanvasResolution(RESOLUTION_AUTO);
-
-    window.addEventListener('resize', () => app.resizeCanvas());
-}
